d7fde1b65ae9ccf114a291e5c8a68cc7a3e1b210
Robin Berjon <robin@berjon.com>
2012-10-04 15:18:35 +0200

    better generation; first batch of link checking fixes

--- scripts/heartbeat.js
+++ scripts/heartbeat.js
@@ -19,7 +19,16 @@ exec("make html", { cwd: rootDir }, function (err, stdout, stderr) {
     console.log(stderr);
     if (err) throw err;
     wrench.copyDirSyncRecursive(pth.join(rootDir, "output/html/"), hbDir);
-    fs.renameSync(pth.join(hbDir, "spec.html"), pth.join(hbDir, "Overview.html"));
+    fs.renameSync(pth.join(hbDir, "spec.html"), pth.join(hbDir, "index.html"));
+    // in every single file in there, replace spec.html with index.html
+    var files = fs.readdirSync(hbDir);
+    for (var i = 0, n = files.length; i < n; i++) {
+        var file = pth.join(hbDir, files[i]);
+        if (!file.match(/\.html$/)) continue;
+        var content = fs.readFileSync(file, "utf-8");
+        content = content.replace(/\bspec\.html\b/g, "index.html");
+        fs.writeFileSync(file, content, "utf-8");
+    }
     console.log([   "The specification has been generated. You may now wish to:"
                 ,   "\t\u2022 Run the link checker on everything (link-checker.js)"
                 ,   "\t\u2022 Run pubrules on everything (pubrules.js)"
--- scripts/link-checker.js
+++ scripts/link-checker.js
@@ -12,14 +12,26 @@ var rootDir = pth.join(__dirname, "..")
 ;
 
 // check, check, check
-var files = fs.readdirSync(hbDir);
+var files = fs.readdirSync(hbDir)
+,   done = 0
+,   total = files.length
+,   checkLink = function () {
+        if (!files.length) return;
+        done++;
+        var file = files.shift();
+        console.log("Checking file " + done + " of " + total + " (" + file + ")");
+        if (file.match(/\.html$/)) {
+            exec("checklink -s -b -q  " + pth.join(hbDir, file), { cwd: hbDir }, function (err, stdout, stderr) {
+                console.log(stdout);
+                if (err) throw err;
+                checkLink();
+            });
+        }
+        else {
+            checkLink();
+        }
+    }
+;
+
 console.log("Checking " + files.length + " files");
-for (var i = 0, n = files.length; i < n; i++) {
-    var file = files[i];
-    console.log("Checking file " + (i + 1) + " of " + files.length + " (" + file + ")");
-    if (!file.match(/\.html$/)) continue;
-    exec("checklink -s -b -q  " + pth.join(hbDir, file), { cwd: hbDir }, function (err, stdout, stderr) {
-        console.log(stdout);
-        if (err) throw err;
-    });
-}
+checkLink();
