85f99fc6595c79dd31b7ce1946ab5f8058adacad
Robin Berjon <robin@berjon.com>
2012-10-04 16:14:32 +0200

    fixup link to author version

--- scripts/heartbeat.js
+++ scripts/heartbeat.js
@@ -35,6 +35,25 @@ exec("make html", { cwd: rootDir }, function (err, stdout, stderr) {
     wrench.copyDirSyncRecursive(pth.join(rootDir, "images/"), imgDir);
     // copy entities stuff
     wrench.copyDirSyncRecursive(pth.join(rootDir, "entities.json"), hbDir);
+    // link to author doc
+    var index = fs.readFileSync(pth.join(hbDir, "index.html"), "utf-8")
+    ,   findDate = /<dt>This Version:<\/dt>\s*<dd><a href="">http:\/\/www.w3.org\/TR\/(\d{4})\/(\w+)-html5-(\d+)/
+    ,   res = findDate.exec(index)
+    ,   year = res[1]
+    ,   status = res[2]
+    ,   date = res[3]
+    ,   fixAuthorLinks = ["index", "single-page"]
+    ;
+    console.log(year, status, date);
+    for (var i = 0, n = fixAuthorLinks.length; i < n; i++) {
+        var page = fixAuthorLinks[i]
+        ,   file = pth.join(hbDir, page + ".html")
+        ,   content = fs.readFileSync(file, "utf-8")
+        ;
+        content = content.replace(/href=(?:")?author\/(?:")?>/, "href='http://www.w3.org/TR/" + year + "/" + status + "-html5-author-" + date + "/'>");
+        fs.writeFileSync(file, content, "utf-8");
+    }
+    
     console.log([   "The specification has been generated. You may now wish to:"
                 ,   "\t\u2022 Run the link checker on everything (link-checker.js)"
                 ,   "\t\u2022 Run pubrules on everything (pubrules.js)"
