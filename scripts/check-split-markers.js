926c41cd9ed6e1b9da4ce4e12a47fb6f844f8ed1
Robin Berjon <robin@berjon.com>
2012-10-15 16:12:52 +0200

    support params for bisect detection

--- scripts/check-split-markers.js
+++ scripts/check-split-markers.js
@@ -3,16 +3,18 @@
 var fs = require("fs")
 ,   pth = require("path")
 ,   bisect = process.argv[2] === "bisect"
+,   target = process.argv[3]
 ,   seen = {}
 ,   line = 0
 ;
 
-fs.readFileSync(pth.join(__dirname, "../source"), "utf8")
+fs.readFileSync(pth.join(__dirname, "../html/source"), "utf8")
     .replace(/(?:<!--(START|END)\s+([-\w]+?)-->|(\n))/g, function (match, status, key, nl, offset) {
         if (nl) {
             line++;
         }
         else {
+            if (target && target !== key) return match;
             if (seen[key]) {
                 if (status === seen[key]) {
                     if (bisect) process.exit(1);
